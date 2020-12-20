efd0c71da1bb4017f4dd7f93ba8b4e424aab888f
Edigleysson Silva <edigleyssonsilva@gmail.com>
2020-12-20 16:12:29 -0300

    feat: fix email tests

--- test/send_mail_test.py
+++ test/send_mail_test.py
@@ -1,4 +1,4 @@
-import smtplib, ssl, os
+import smtplib, ssl, os, sys
 from email import encoders
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -12,14 +12,15 @@ EMAIL_USER = os.getenv('EMAIL_USER')
 EMAIL_PASSWORD = os.getenv('EMAIL_PASSWORD')
 EMAIL_FROM = os.getenv('EMAIL_FROM')
 EMAIL_TO = os.getenv('EMAIL_TO')
+FROM_NAME = os.getenv('FROM_NAME')
 
-# # Create a secure SSL context
+# Create a secure SSL context
 context = ssl.create_default_context()
 
 msg = MIMEMultipart()
-msg['From'] = EMAIL_FROM
+msg['From'] = '{}, <{}>'.format(FROM_NAME, EMAIL_FROM)
 msg['To'] = EMAIL_TO
-msg['Subject'] = 'Email com anexo'
+msg['Subject'] = 'Email com anexo 2'
 # body = 'Testando coisas com python em texo'
 body_html = 'Testando envio de anexo no e-mail'
 # msg.attach(MIMEText(body, 'plain'))
@@ -58,6 +59,9 @@ try:
   # TODO: Send email here
 except Exception as e:
   # Print any error messages to stdout
-  print(e)
+  # print(e)
+  exc_type, exc_obj, exc_tb = sys.exc_info()
+  fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+  print(exc_type, fname, exc_tb.tb_lineno)
 finally:
   server.quit()
